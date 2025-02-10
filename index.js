c9fbbff3083c8683af8b0c9b4ed107b0cf9db8b4
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-02-10 11:50:25 +0100

    installed venom

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
-import mongoose from "mongoose";
-import AdmZip from "adm-zip";
-import fsExtra from "fs-extra";
-import streamifier from "streamifier"; // For converting Buffer to stream
 
 dotenv.config();
 
